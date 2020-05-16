405a8a4e79488abc668069628b900ff831e60db7
mmy83 <mmy83@126.com>
2020-05-16 18:27:12 +0800

    notebook列表功能样式

--- src/layout/components/NoteBook/Item.vue
+++ src/layout/components/NoteBook/Item.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="item-sub">
     <span class="title" @click="clickNoteBook">{{ title }}</span>
-    <i class="el-icon-setting left-setting" />
+
+    <el-popover
+      placement="right"
+      width="115"
+      trigger="click">
+      <table>
+        <tr>
+          <td style="width: 15px"><i class="fa fa-sitemap"></i></td>
+          <td style="width:125px;background: #ffffff">添加子笔记本</td>
+        </tr>
+        <tr>
+          <td><i class="fa fa-edit"></i></td>
+          <td style="background: #ffffff">重命名</td>
+        </tr>
+        <tr>
+          <td><i class="fa fa-trash"></i></td>
+          <td style="background: #ffffff"><span>删除</span></td>
+        </tr>
+      </table>
+
+<!--     <i class="el-icon-setting left-setting" slot="reference"/>-->
+      <i class="fa fa-gear left-setting" slot="reference"/>
+    </el-popover>
     <span class="count">{{ count }}</span>
   </div>
 </template>
@@ -18,6 +40,24 @@ export default {
     count: {
       type: Number,
       default: 0
+    }
+  },
+  data() {
+    return {
+      visible: false,
+      gridData: [{
+        icon: 'icon',
+        name: '王小虎',
+      }, {
+        icon: 'icon',
+        name: '王小虎',
+      }, {
+        icon: 'icon',
+        name: '王小虎',
+      }, {
+        icon: 'icon',
+        name: '王小虎',
+      }]
     }
   },
   // render(h, context) {
