ea599faedac94bec0b18665388cc860379f2940a
mmy83 <mmy83@126.com>
2020-06-13 16:24:25 +0800

    修改笔记内容展示及编辑页面样式

--- mock/note.js
+++ mock/note.js
@@ -1,24 +1,24 @@
 
 const notes = [
-  { 'id': 1, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 1', 'created': '2020-05-09 12:12:12' },
-  { 'id': 2, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 2', 'created': '2020-05-09 12:12:12' },
-  { 'id': 3, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 3', 'created': '2020-05-09 12:12:12' },
-  { 'id': 4, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 4', 'created': '2020-05-09 12:12:12' },
-  { 'id': 5, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 5', 'created': '2020-05-09 12:12:12' },
-  { 'id': 6, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 6', 'created': '2020-05-09 12:12:12' },
-  { 'id': 7, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 7', 'created': '2020-05-09 12:12:12' },
-  { 'id': 8, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 8', 'created': '2020-05-09 12:12:12' },
-  { 'id': 9, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 9', 'created': '2020-05-09 12:12:12' },
-  { 'id': 10, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 10', 'created': '2020-05-09 12:12:12' },
-  { 'id': 11, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 11', 'created': '2020-05-09 12:12:12' },
-  { 'id': 12, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 12', 'created': '2020-05-09 12:12:12' },
-  { 'id': 13, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 13', 'created': '2020-05-09 12:12:12' },
-  { 'id': 14, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 14', 'created': '2020-05-09 12:12:12' },
-  { 'id': 15, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 15', 'created': '2020-05-09 12:12:12' },
-  { 'id': 16, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 16', 'created': '2020-05-09 12:12:12' },
-  { 'id': 17, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 17', 'created': '2020-05-09 12:12:12' },
-  { 'id': 18, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 18', 'created': '2020-05-09 12:12:12' },
-  { 'id': 19, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 19', 'created': '2020-05-09 12:12:12' }
+  { 'id': 1, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 1', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 1, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 2, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 2', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 4, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 4', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 3, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 3', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 5, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 5', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 6, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 6', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 7, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 7', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 8, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 8', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 9, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 9', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 10, 'notebook': { 'id': 1, 'notebook_name': 'my book 1' }, 'title': 'my note 10', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 11, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 11', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 12, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 12', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 13, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 13', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 14, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 14', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 15, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 15', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 16, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 16', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 17, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 17', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 18, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 18', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] },
+  { 'id': 19, 'notebook': { 'id': 2, 'notebook_name': 'my book 2' }, 'title': 'my note 19', 'created': '2020-05-09 12:12:12', 'updated': '2020-05-09 12:12:12', 'content': 'my note 1 count', 'type': 'edit', 'ismd': 0, 'tags': [{ 'id': 1, 'tag_name': '001' }, { 'id': 2, 'tag_name': '002' }] }
 ]
 
 export default [
--- src/views/note/index.vue
+++ src/views/note/index.vue
@@ -1,16 +1,136 @@
 <template>
-  <h1>
-    欢迎光临!!!!!!!!!!!!
-    {{$route.params.id}}---
-    {{currentNote.title}}
-  </h1>
+  <div class="note-page">
+    <template v-if="currentNote.type=='edit' && currentNote.ismd == 0">
+      <div class="note-tag">
+        <ul class="note-tags">
+          <li v-for="item in currentNote.tags" :key="item.id">{{item.tag_name}} <i class="fa fa-remove"></i></li>
+        </ul>
+        <div class="note-tools">
+          <i class="fa fa-edit"></i>
+          <i class="fa fa-save"></i>
+          <i class="fa fa-paperclip"></i>
+          <i class="fa fa-history"></i>
+        </div>
+      </div>
+      <div class="note-title">
+        <input v-model="currentNote.title" />
+      </div>
+      <div class="note-content">
+        <tinymce v-model="currentNote.content" :height="300" />
+      </div>
+
+    </template>
+    <template v-if="currentNote.type=='edit' && currentNote.ismd==1">
+      <div class="note-tag">
+        <ul class="note-tags">
+          <li v-for="item in currentNote.tags" :key="item.id"> {{item.tag_name}} <i class="fa fa-remove"></i></li>
+        </ul>
+        <div class="note-tools">
+          <i class="fa fa-edit"></i>
+          <i class="fa fa-save"></i>
+          <i class="fa fa-paperclip"></i>
+          <i class="fa fa-history"></i>
+        </div>
+      </div>
+      <div class="note-title">
+        <input v-model="currentNote.title" />
+      </div>
+      <div class="note-content">
+        <markdown-editor ref="markdownEditor" v-model="currentNote.content" height="300px" />
+      </div>
+    </template>
+    <template v-if="!currentNote.type">
+      <div class="note-tag">
+        <ul class="note-tags">
+          <li v-for="item in currentNote.tags" :key="item.id">{{item.tag_name}} <i class="fa fa-remove"></i></li>
+        </ul>
+        <div class="note-tools">
+          <i class="fa fa-edit"></i>
+          <i class="fa fa-save"></i>
+          <i class="fa fa-paperclip"></i>
+          <i class="fa fa-history"></i>
+        </div>
+      </div>
+      <div class="note-title">
+        {{currentNote.title}}
+      </div>
+      <div class="note-time">Created：{{currentNote.created }}&nbsp;&nbsp;&nbsp;&nbsp;Updated：{{currentNote.updated}}</div>
+      <div class="note-content">
+        {{currentNote.content}}
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+        aaaaaa
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+        mmmmmmmmm
+      </div>
+    </template>
+
+  </div>
+
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Tinymce from '@/components/Tinymce'
+import MarkdownEditor from '@/components/MarkdownEditor'
 
 export default {
-
+  components: { Tinymce, MarkdownEditor },
   computed: {
     ...mapGetters([
       'sidebar',
